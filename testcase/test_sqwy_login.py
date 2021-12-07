77bad8f4798693618dea260f9baff0f5b8c4f186
chailin <903369232@qq.com>
2021-12-07 19:24:40 +0800

    allure项目

--- testcase/test_sqwy_login.py
+++ testcase/test_sqwy_login.py
@@ -24,7 +24,7 @@ class Test_login:
             currdir = 'D:/Program Files/JetBrains/PyCharm Community Edition 2020.2/pytest_allure/share'
             page = Loginpage(self.driver)
             page.get_picture(currdir)
-            page.search_username("18511067360")
+            page.search_username("13734206025")
             page.search_password("bgfg1000lbfwlXP#")
             page.sleep(2)
             page.search_code(currdir)
@@ -34,22 +34,22 @@ class Test_login:
             if url != 'http://111.53.13.251:3100/loginPage?error':
                 # url = 'http://sso.wt.com:3100/loginPage?error'
                 # assert page.get_url() == 'http://sqwytst.wt.com:14352/dashboard'
-                assert page.get_title() == '首页 - 智慧网格综合管理平台'
+                assert page.get_title() == '智慧网格综合管理平台'
                 break
             else:
                 page.get_picture(currdir)
                 continue
         # 获取岗位弹窗的标题 传文字定位信息  未弹出选择岗位的页面，则不需要执行选择
-        if page.get_text('') == '':
+        if page.get_text('/html/body/div[1]/div/div[2]/div/div/div[1]/span') =='':
             pass
         else:
             page.search_job('/html/body/div[1]/div/div[2]/div/div/div[2]/div[2]/div[3]/table/tbody/tr/td[2]/div/label/span[1]/span')
             page.login_job('/html/body/div[1]/div/div[2]/div/div/div[3]/div/button[2]/span')
 
-    # # 环境清理
-    # @classmethod
-    # def teardown_class(cls):
-    #     cls.driver.quit()
+    # 环境清理
+    @classmethod
+    def teardown_class(cls):
+        cls.driver.quit()
 
 
 if __name__ == '__main__':
