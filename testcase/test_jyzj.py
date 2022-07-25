340fce3e89b4acfd28c3a3e5af53a31a5e3bda4c
chailin <903369232@qq.com>
2022-07-25 10:18:20 +0800

    调整登录，增加用户授权的步骤

--- testcase/test_jyzj.py
+++ testcase/test_jyzj.py
@@ -11,7 +11,7 @@ class Test_login:
     @classmethod
     def setup_class(cls):
         cls.driver = webdriver.Chrome(executable_path=r'D:\Project\pytest_allure\chromedriver.exe')
-        cls.driver.get('http://www.pingcheng.gov.cn/page/admin_safetyplatform/')
+        cls.driver.get('http://111.53.13.252/admin_jyzj/')
         cls.driver.find_element_by_css_selector('.login_btn').click()
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
@@ -20,11 +20,11 @@ class Test_login:
     @allure.feature('若验证码错误，可循环登录十次')
     def test_login(self):
         for i in range(10):
-            currdir = 'D:/Program Files/JetBrains/PyCharm Community Edition 2020.2/pytest_allure/share'
+            currdir = 'D:/Project/pytest_allure/share'
             page = Loginpage(self.driver)
             page.get_picture(currdir)
-            page.search_username("18035285063")
-            page.search_password("bgfg1000lbfwlXP#")
+            page.search_username("18511067360")
+            page.search_password("F&iijuq1vX&PR")
             page.sleep(2)
             page.search_code(currdir)
             page.click_login()
@@ -32,8 +32,13 @@ class Test_login:
             url = page.get_url()
             if url != 'http://111.53.13.251:3100/loginPage?error':
                 # assert page.get_url() == 'http://sqwytst.wt.com:14352/dashboard'
-                assert page.get_title() == '“安易得”安全生产综合监管平台'
-                break
+                if page.get_title() == '用户授权':
+                    self.driver.find_element_by_xpath('//*[@id="login"]').click()
+                    assert page.get_title() == '教育之家'
+                    break
+                else:
+                    assert page.get_title() == '教育之家'
+                    break
             else:
                 page.get_picture(currdir)
                 continue
@@ -75,4 +80,4 @@ class Test_login:
 
 
 if __name__ == '__main__':
-    pytest.main(['-s', '-v', 'tes_anjian.py'])
+    pytest.main(['-s', '-v', 'test_jyzj.py'])
